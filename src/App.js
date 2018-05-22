3bc4ca259c613fbb8abf3fefb933d66c01a74185
Ernesto Ramírez <ernesto.ramirez88@gmail.com>
2018-05-22 02:38:18 -0500

    Added base header

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 import React, { Component } from 'react';
-import Logo from './components/atoms/Logo';
-import Button from './components/atoms/Button';
-import MenuItem from '@material-ui/core/MenuItem';
-import { MenuButton, Dropdown } from './components/atoms/Select';
+
+import Header from './components/organisms/header';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <Logo />
-          <Button text="Ayuda" />
-          <h1 className="App-title">Welcome to React</h1>
-          <MenuButton
-            buttonText="Wallet"
-            menuItems={[{ text: 'lorem', onClick: () => {} }, { text: 'ipsum', onClick: () => {} }]}
-          />
-          <Dropdown value="chart">
-            <MenuItem value="chart">c</MenuItem>
-            <MenuItem value="deepmarket">d</MenuItem>
-          </Dropdown>
-        </header>
+        <Header />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/components/organisms/header/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Avatar from '@material-ui/core/Avatar';
+import Toolbar from '@material-ui/core/Toolbar';
+import MenuItem from '@material-ui/core/MenuItem';
+import Typography from '@material-ui/core/Typography';
+import Switch from '@material-ui/core/Switch';
+import { withStyles } from '@material-ui/core/styles';
+
+import Logo from '../../atoms/Logo';
+import Button from '../../atoms/Button';
+import { MenuButton, Dropdown } from '../../atoms/Select';
+
+const styles = {
+  flex: {
+    flex: 1,
+    textAlign: 'end',
+  },
+};
+
+const Header = props => {
+  const { classes } = props;
+  return (
+    <header className="App-header">
+      <AppBar position="static" color="default">
+        <Toolbar>
+          <Logo />
+          <Typography variant="title" color="inherit">
+            | Exchange
+          </Typography>
+          <Typography variant="title" color="inherit" className={classes.flex}>
+            1 BTC = 000,000.00 MXN |
+          </Typography>
+          <MenuButton
+            buttonText="Wallet"
+            menuItems={[{ text: 'lorem', onClick: () => {} }, { text: 'ipsum', onClick: () => {} }]}
+          />
+          <MenuButton
+            buttonText="Exchange"
+            menuItems={[{ text: 'lorem', onClick: () => {} }, { text: 'ipsum', onClick: () => {} }]}
+          />
+          <Button text="Ayuda" />
+          <Avatar> </Avatar>
+          <MenuButton
+            buttonText="Usuario"
+            menuItems={[{ text: 'lorem', onClick: () => {} }, { text: 'ipsum', onClick: () => {} }]}
+          />
+          <Switch value="checkedA" />
+        </Toolbar>
+      </AppBar>
+    </header>
+  );
+};
+
+export default withStyles(styles)(Header);
